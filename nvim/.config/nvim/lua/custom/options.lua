9ea87bf407047e93e5a7469429438b5b10fd53f9
Chahat <chahat@disroot.org>
2026-03-13 03:10:42 -0500

    Configure visual and editor options
    Enable relative line numbers, set custom list characters, and adjust scrolling and wrapping settings for an improved editing experience.

--- nvim/.config/nvim/lua/custom/options.lua
+++ nvim/.config/nvim/lua/custom/options.lua
@@ -6,8 +6,7 @@
 -- Make line numbers default
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -47,13 +46,8 @@ vim.o.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
---
---  Notice listchars is set using `vim.opt` instead of `vim.o`.
---  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
---   See `:help lua-options`
---   and `:help lua-guide-options`
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = 'tab:^ ,nbsp:¬,extends:»,precedes:«,trail:•'
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
@@ -62,7 +56,13 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
+vim.o.scrolloff = 2
+
+-- show a column at 80 characters as a guide for long lines
+vim.opt.colorcolumn = '80'
+
+-- never show me line breaks if they're not there
+vim.opt.wrap = false
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
