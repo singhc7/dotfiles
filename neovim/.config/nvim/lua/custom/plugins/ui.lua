ee3aee8593e0e939b789c791362880902042f5fb
Chahat <chahat@disroot.org>
2026-04-27 03:45:01 -0500

    Replace lightline with mini.statusline and add mini.pairs

--- neovim/.config/nvim/lua/custom/plugins/ui.lua
+++ neovim/.config/nvim/lua/custom/plugins/ui.lua
@@ -20,49 +20,6 @@ return {
     opts = { signs = false },
   },
 
-  -- nice bar at the bottom
-  {
-    'itchyny/lightline.vim',
-    lazy = false, -- also load at start since it's UI
-    config = function()
-      -- no need to also show mode in cmd line when we have bar
-      vim.o.showmode = false
-      vim.g.lightline = {
-        colorscheme = 'one',
-        active = {
-          left = {
-            { 'mode', 'paste' },
-            { 'readonly', 'filename', 'modified' },
-          },
-          right = {
-            { 'lineinfo' },
-            { 'percent' },
-            { 'fileencoding', 'filetype' },
-          },
-        },
-        component_function = {
-          filename = 'LightlineFilename',
-        },
-      }
-      function _G.LightlineFilenameInLua(opts)
-        if vim.fn.expand '%:t' == '' then
-          return '[No Name]'
-        else
-          return vim.fn.getreg '%'
-        end
-      end
-      -- https://github.com/itchyny/lightline.vim/issues/657
-      vim.api.nvim_exec(
-        [[
-				function! g:LightlineFilename()
-					return v:lua.LightlineFilenameInLua()
-				endfunction
-				]],
-        true
-      )
-    end,
-  },
-
   { -- Collection of various small independent plugins/modules
     'nvim-mini/mini.nvim',
     config = function()
@@ -71,6 +28,12 @@ return {
 
       -- Add/delete/replace surroundings
       require('mini.surround').setup()
+
+      -- Auto-pairs (replaces nvim-autopairs; same plugin family)
+      require('mini.pairs').setup()
+
+      -- Statusline (replaces lightline; same plugin family as the rest)
+      require('mini.statusline').setup { use_icons = vim.g.have_nerd_font }
     end,
   },
 }
