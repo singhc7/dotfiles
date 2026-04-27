ed72aa21d7bc475a08ff3011d11ad714a38a3778
Chahat <chahat@disroot.org>
2026-04-27 03:44:50 -0500

    Skip non-modifiable buffers in nvim-lint

--- neovim/.config/nvim/lua/custom/plugins/lint.lua
+++ neovim/.config/nvim/lua/custom/plugins/lint.lua
@@ -1,22 +1,24 @@
 return {
-	{
-		"mfussenegger/nvim-lint",
-		event = { "BufReadPre", "BufNewFile" },
-		config = function()
-			local lint = require("lint")
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require('lint')
 
-			lint.linters_by_ft = {
-				sh = { "shellcheck" },
-				bash = { "shellcheck" },
-				python = { "ruff" },
-			}
+      lint.linters_by_ft = {
+        sh = { 'shellcheck' },
+        bash = { 'shellcheck' },
+        python = { 'ruff' },
+      }
 
-			vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
-				group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-				callback = function()
-					lint.try_lint()
-				end,
-			})
-		end,
-	},
+      local lint_augroup = vim.api.nvim_create_augroup('nvim-lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          -- Skip read-only / non-modifiable buffers (e.g. LSP hover popups).
+          if vim.bo.modifiable then lint.try_lint() end
+        end,
+      })
+    end,
+  },
 }
